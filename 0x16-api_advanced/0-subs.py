3c372b0789b34ee2752579818cf0bea9f4ca5c5d
GreenMoCh <125460183+GreenMoCh@users.noreply.github.com>
2024-01-16 21:43:48 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,10 @@ def number_of_subscribers(subreddit):
     """Return the number of subs for a give subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyRedditBot/1.0'}
-    response = requests.get(url, headres=headers)
+    response = requests.get(url, headres=headers, allow_redirets=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        subs_count = data['{}']['{}'].format(data, subscribers) 
-        return subs_count
-    elif response.status_code == 404:
+    if response.status_code == 404:
         return 0
+    data = response.json().get("data") 
+    return data.get("subscribers")
+    
