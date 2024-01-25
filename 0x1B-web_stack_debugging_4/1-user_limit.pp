61b8e2f9328aa2dcf281433d936e049387adcd33
GreenMoCh <moadchabaa0@gmail.com>
2024-01-25 08:29:38 -0800

    0x1B. Web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# High amount of opened files
+
+exec {'replace-1':
+    provider => shell,
+    command => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+    before => Exec['replace-2'],
+}
+
+exec {'replace-2':
+    provider => shell,
+    comand => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
